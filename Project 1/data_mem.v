3aa7d803062f6ab08ced57264c9b3ac7e70681a6
Nived151 <sainived1511@gmail.com>
2023-01-26 19:54:25 -0600

    new files and initialization

---
+++ Project 1/data_mem.v
@@ -0,0 +1,7 @@
+module data_mem();
+input address_bus[0:7];
+input data_in[0:7];
+reg data_out[0:7];
+input r_w;
+
+endmodule
---
+++ Project 1/program_mem.v
@@ -0,0 +1,5 @@
+module program_mem();
+input program_mem_address_bus[0:7];
+reg program_mem_data_out[0:15];
+
+endmodule
