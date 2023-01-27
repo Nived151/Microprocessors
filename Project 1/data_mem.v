05d1a1e397ab18eb0c21744bb57b08d0b430cfb4
Nived151 <sainived1511@gmail.com>
2023-01-26 20:08:51 -0600

    completed data_mem and created testbench for it

--- Project 1/data_mem.v
+++ Project 1/data_mem.v
@@ -1,7 +1,22 @@
 module data_mem();
+input clk;
 input address_bus[0:7];
 input data_in[0:7];
-reg data_out[0:7];
+output data_out[0:7];
 input r_w;
+reg [0:7]ram[0:255];
+integer i;
+
+initial begin
+  for (i=0; i<256 ; i=i+1)
+  ram[i] = 0;
+end
+
+always @(posedge clk) begin
+  if (r_w == 1)
+  ram[address_bus] <= data_in;
+  else
+  data_out <= ram[address_bus]
+end
 
 endmodule
