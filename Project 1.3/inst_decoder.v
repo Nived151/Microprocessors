e99fb176a282f1a4a5886ad100b2c9d50684c51b
Nived151 <sainived1511@gmail.com>
2023-02-14 20:23:13 -0600

    UPDATED PARA

--- Project 1.3/inst_decoder.v
+++ Project 1.3/inst_decoder.v
@@ -17,14 +17,45 @@ output CS; //constant Unit
 output OE; //output enable
 
 parameter NOP = 16'd0;
+parameter MOVA = 16'd0;
+parameter ADD= 16'd0;
+parameter SUB = 16'd0;
+parameter AND = 16'd0;
+parameter OR = 16'd0;
+parameter XOR = 16'd0;
+parameter NOT = 16'd0;
+parameter ADI = 16'd0;
+parameter SBI = 16'd0;
+parameter ANI = 16'd0;
+parameter ORI = 16'd0;
+parameter XRI = 16'd0;
+parameter AIU = 16'd0;
+parameter SIU = 16'd0;
+parameter MOVB = 16'd0;
+parameter LSR = 16'd0;
+parameter LSL = 16'd0;
+parameter LD = 16'd0;
+parameter ST = 16'd0;
+parameter JMR = 16'd0;
+parameter SLT = 16'd0;
+parameter NOP = 16'd0;
+parameter BZ = 16'd0;
+parameter BNZ = 16'd0;
+parameter JMP = 16'd0;
+parameter JML = 16'd0;
+parameter IN = 16'd0;
+parameter INK = 16'd0;
+parameter OUT = 16'd0;
+
+
 
 always @(*)begin
   
 opcode=Instruction_in[16:12];
-MD=2'b00; BS=2'b00; PS=
+MD=2'b00; BS=2'b00; PS=1'b0; MW=1'b0; MB=1'b0; MA=1'b0; CS=1'b0; OE=1'b0;
 case(opcode)
     NOP:begin
-    
+    RW = 1'b0;FS=4'd0;
     end
 end
 
