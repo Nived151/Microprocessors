e2d78b801d050e29da040bba5c1adc9e437d02a0
Nived151 <sainived1511@gmail.com>
2023-02-15 16:41:43 -0600

    inst update

--- Project 1.2/register.v
+++ Project 1.2/register.v
@@ -1,15 +1,25 @@
-module register();
+module Register_File(input clk, input rst, input WR, input [2:0] DA, input [7:0] Data_in,
+                    input [3:0] BA, input [2:0] AA, output  [7:0] Data_A, output  [7:0] Data_B);
 
-reg [7:0]register;
+reg [7:0] R[15:0]; // array to store the data of all 16 registers
+integer i;
+always @(posedge clk or posedge rst)
+begin
+    if(rst) // reset all registers to zero
+    begin
+        for(i=0; i<=15; i=i+1)begin
+            R[i] <= 8'b0;
+        end
+    end
+    else if(WR) // write data to specified register
+    begin
+        R[DA] <= Data_in;
+    end
+end
 
-input [2:0]AA;
-input [3:0]BA;
-input [2:0]DA;
-input [7:0]data_in;
-input wr;
-input clk;
-input rst;
-output data_a;
-output data_b;
+assign Data_A = R[AA];
+assign Data_B = R[BA];
+
+// Note that R0 cannot be written. Your code should avoid this case by defining appropriate logic
 
 endmodule
--- Project 1.3/inst_decoder.v
+++ Project 1.3/inst_decoder.v
@@ -16,38 +16,23 @@ output [2:0]SH; //shift number
 output CS; //constant Unit
 output OE; //output enable
 
-parameter NOP = 16'd0;
-parameter MOVA = 16'd0;
-parameter ADD= 16'd0;
-parameter SUB = 16'd0;
-parameter AND = 16'd0;
-parameter OR = 16'd0;
-parameter XOR = 16'd0;
-parameter NOT = 16'd0;
-parameter ADI = 16'd0;
-parameter SBI = 16'd0;
-parameter ANI = 16'd0;
-parameter ORI = 16'd0;
-parameter XRI = 16'd0;
-parameter AIU = 16'd0;
-parameter SIU = 16'd0;
-parameter MOVB = 16'd0;
-parameter LSR = 16'd0;
-parameter LSL = 16'd0;
-parameter LD = 16'd0;
-parameter ST = 16'd0;
-parameter JMR = 16'd0;
-parameter SLT = 16'd0;
-parameter NOP = 16'd0;
-parameter BZ = 16'd0;
-parameter BNZ = 16'd0;
-parameter JMP = 16'd0;
-parameter JML = 16'd0;
-parameter IN = 16'd0;
-parameter INK = 16'd0;
-parameter OUT = 16'd0;
-
-
+parameter NOP = 5'd0;
+parameter ADD= 5'd1;
+parameter OUT = 5'd2;
+parameter SLT = 5'd3;
+parameter AND = 5'd4;
+parameter LD = 5'd5;
+parameter SBI = 5'd6;
+parameter LSL = 5'd7;
+parameter IN = 5'd8;
+parameter XRI = 5'd9;
+parameter ADI = 5'd10;
+parameter BZ = 5'd11;
+parameter BNZ = 5'd212;
+parameter ST = 5'd13;
+parameter MOVA = 5'd14;
+parameter JMP = 5'd15;
+parameter JML = 5'd16;
 
 always @(*)begin
   
@@ -56,6 +41,12 @@ MD=2'b00; BS=2'b00; PS=1'b0; MW=1'b0; MB=1'b0; MA=1'b0; CS=1'b0; OE=1'b0;
 case(opcode)
     NOP:begin
     RW = 1'b0;FS=4'd0;
+    end
+    ADD: begin
+      RW = 1'b1; FS=4'd1; AA=Instruction_in[8:6]; BA=Instruction_in[5:3];DA=Instruction_in[11:9];
+    end
+    OUT:begin
+      RW = 1'b0; FS = 4'b2; OE = 1'b1;
     end
 end
 
