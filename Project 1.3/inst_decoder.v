93631c5ea2dbe99e8c7b2a8fb3665421dc343edf
Nived151 <sainived1511@gmail.com>
2023-02-27 11:46:58 -0600

    modified inst decover a week back

--- Project 1.3/inst_decoder.v
+++ Project 1.3/inst_decoder.v
@@ -38,57 +38,58 @@ parameter JML = 5'd16;
 
 always @(*)begin
   
-opcode <=Instruction_in[16:12];
-MD=2'b00; BS=2'b00; PS=1'b0; MW=1'b0; MB=1'b0; MA=1'b0; CS=1'b0; OE=1'b0;
+opcode<=Instruction_in[16:12];
+$display(Instruction_in[16:12]);
+MD=2'b00; BS=2'b00; PS=1'b0; MW=1'b0; MB=1'b0; MA=1'b0; CS=1'b0; OE=1'b0; DA=Instruction_in[11:9]; AA=Instruction_in[8:6];
 case(opcode)
     NOP:begin
     RW = 1'b0;FS=4'd0;
     end
     ADD: begin
       $display("addcase");
-      RW = 1'b1; FS=4'd1; AA=Instruction_in[8:6]; BA=Instruction_in[5:3];DA=Instruction_in[11:9];
+      RW = 1'b1; FS=4'b0001;  BA=Instruction_in[5:3];
     end
     OUT:begin
-      RW = 1'b0; FS = 4'd2; OE = 1'b1; AA=Instruction_in[8:6]; BA=Instruction_in[5:3];DA=Instruction_in[11:9];
+      RW = 1'b0; FS = 4'd2; OE = 1'b1;  BA=Instruction_in[5:3];
     end
     SLT:begin
-      RW=1'b1;MD=2'b10; FS=4'd3; AA=Instruction_in[8:6]; BA=Instruction_in[5:3];DA=Instruction_in[11:9];
+      RW=1'b1;MD=2'b10; FS=4'd3;  BA=Instruction_in[5:3];
     end
     AND: begin
-      RW = 1'b1; FS = 4'd4; OE = 1'b0; AA=Instruction_in[8:6]; BA=Instruction_in[5:3];DA=Instruction_in[11:9];
+      RW = 1'b1; FS = 4'd4; OE = 1'b0;  BA=Instruction_in[5:3];
     end
     LD: begin
-      RW = 1'b1; FS = 4'd5; MD = 2'b01; AA=Instruction_in[8:6]; DA=Instruction_in[11:9]; 
+      RW = 1'b1; FS = 4'd5; MD = 2'b01;   
     end
     SBI: begin
-      // immediate
+      RW = 1'b1; FS = 4'd6; BA=Instruction_in[5:3];// immediate
     end
     LSL: begin
-      RW = 1'b1; FS = 4'd7; AA=Instruction_in[8:6]; BA=Instruction_in[5:3];DA=Instruction_in[11:9];
+      RW = 1'b1; FS = 4'd7;  BA=Instruction_in[5:3]; SH=Instruction_in[2:0];
     end
     IN: begin
-      // idk
+      RW = 1'b1; FS = 4'd8;
     end
     XRI: begin
-      // immedieate
+      RW = 1'b1; FS = 4'd9; MB = 1'b1; BA=Instruction_in[5:3];
     end
     ADI: begin
-      // immediate
+      RW = 1'b1; FS = 4'd10; // immediate
     end
     BZ:begin
-      // 
+      RW = 1'b0; FS = 4'd11; BS = 2'b01; MB = 1'b1; CS = 1'b1; DA = 3'b000;   
     end
     BNZ: begin
-      //
+      RW = 1'b0; FS = 4'd12; BS = 2'b01; PS = 1'b1; MB = 1'b1; CS = 1'b1; DA = 3'b000;
     end
     ST: begin
-     //
+      RW = 1'b0; FS = 4'd0; MW = 1'b1; BA = Instruction_in[5:3];
     end
     MOVA: begin
-      //
+      RW = 1'b0; FS = 4'd13;//
     end
     JMP: begin
-      //
+      RW = 1'b0; FS=4'd0; BS = 2'b11; MB = 1'b1; CS = 1'b1; DA = 3'b000; AA = 3'b000; BA = 3'b000;
     end
     endcase
 end
--- Project 1.3/inst_decoder_tb.v
+++ Project 1.3/inst_decoder_tb.v
@@ -20,9 +20,10 @@ wire OE;
 inst_decoder uut(.Instruction_in(Instruction_in), .DA(DA), .AA(AA),.BA(BA), .BS(BS), .PS(PS), .MW(MW), .RW(RW), .MA(MA), .MB(MB), .MD(MD), .FS(FS), .SH(SH), .CS(CS), .OE(OE) );
 
  initial begin
-    Instruction_in = 17'b0001101100100;
-    /*$dumpfile("inst_decoder.vcd");
-    $dumpvars(0, inst_decoder_tb);*/
+    Instruction_in = 17'd6736;
+    $dumpfile("inst_decoder.vcd");
+    $dumpvars(0, inst_decoder_tb);
+    $display(FS);
  end
 
 
