b76dc4d37f4d931f6098538bb65713905df43aa9
Nived151 <sainived1511@gmail.com>
2023-03-10 13:45:01 -0600

    updated to final

---
+++ Project 1/mcu.v
@@ -0,0 +1,44 @@
+`include "data_mem.v"
+`include "inst_mem.v" 
+`include "cpu.v"
+
+module mcu(
+    input rst,
+    input clk,
+    input [7:0] mcu_input,
+    input [7:0] KB,
+    output [7:0] Output_LSB,
+    output [7:0] Output_MSB
+);
+
+wire [7:0] Address_DM_wire;
+wire [7:0] Data_in_DM_wire;
+wire [7:0] Data_out_DM_wire;
+wire [7:0] Address_IM_wire;
+wire [16:0] Inst_in_wire;
+wire data_mem_clk_wire;
+
+data_memoryfile conn(
+    //.rst(rst),
+    .data_mem_clk(clk),
+    .data_mem_address(Address_DM_wire),
+    .data_mem_data_in(Data_in_DM_wire),
+    .data_mem_data_out(Data_out_DM_wire)
+);
+
+instruction_memory conn1(
+    .addr_in(Address_IM_wire),
+    .data_out(Inst_in_wire)
+);
+
+cpu conn2(
+    .rst(rst),
+    .clk(clk),
+    .cpu_input(mcu_input),
+    .KB(KB),
+    .Output_LSB(Output_LSB),
+    .Output_MSB(Output_MSB)
+);
+
+
+endmodule
---
+++ Project 1/minsys.v
@@ -0,0 +1,36 @@
+`include "mcu.v"
+`include "mcu_io.v"
+
+module minsys(
+    input clk,
+    input rst,
+    input EI,
+    input [7:0] SW,
+    output [9:0] vga_cont,
+    input [3:0] Keypad_rows,
+    output [3:0] Keypad_cols 
+);
+
+wire [7:0] sw_pos_wire;
+wire [7:0] Decoded_keyboard_wire;
+wire [7:0] Grid_Position_wire;
+wire [7:0] Color_wire;
+
+mcu conn3(
+    .rst(rst),
+    .clk(clk),
+    .mcu_input(sw_pos_wire),
+    .KB(Decoded_keyboard_wire),
+    .Output_MSB(Grid_Position_wire),
+    .Output_LSB(Color_wire)
+);
+
+mcu_io conn4(
+    .EIs(EI),
+    .SW(SW),
+    .vga_cont(vga_cont),
+    .Keypad_rows(Keypad_rows),
+    .Keypad_cols(Keypad_cols)
+);
+
+endmodule
